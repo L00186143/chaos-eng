1d456ab848137619dc90ddcca7ce4232ddc8ce91
stereynoatu <L00186143@atu.ie>
2024-03-04 18:00:10 +0000

    update sec group id

--- experiment/chaos-experiment.py
+++ experiment/chaos-experiment.py
@@ -66,7 +66,7 @@ def main():
         print("Auto Scaling Group name not found.")
         return
 
-    security_group_id = 'sg-0182567f6884ec242'
+    security_group_id = 'sg-0c0d9ff0204674cac'
     random_action = random.choice(['terminate_instance', 'toggle_rule'])
 
     if random_action == 'terminate_instance':
